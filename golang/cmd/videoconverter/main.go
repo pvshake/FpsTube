bf24c690b11303959c4a3c6b0d8d82189fbfecde
pvshake <pvshakeu@gmail.com>
2024-10-29 16:00:04 -0300

    aula 2 finalizada, construindo o transcoding de video com golang localmente

---
+++ golang/cmd/videoconverter/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"fpstube/internal/converter"
+	"log/slog"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func connectPostgres() (*sql.DB, error) {
+	user := getEnvOrDefault("POSTGRES_USER", "user")
+	password := getEnvOrDefault("POSTGRES_PASSWORD", "password")
+	dbname := getEnvOrDefault("POSTGRES_DB", "converter")
+	host := getEnvOrDefault("POSTGRES_HOST", "postgres")
+	sslmode := getEnvOrDefault("POSTGRES_SSLMODE", "disable")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s", user, password, dbname, host, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		slog.Error("Error connecting to database", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		slog.Error("Error pinging database", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	slog.Info("Connected to Postgres successfully")
+	return db, nil
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func main() {
+	db, err := connectPostgres()
+	if err != nil {
+		panic(err)
+	}
+	vc := converter.NewVideoConverter(db)
+	vc.Handle([]byte(`{"video_id": 1, "path": "/media/uploads/1"}`))
+}
---
+++ golang/internal/converter/task.go
@@ -0,0 +1,152 @@
+package converter
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type VideoConverter struct {
+	db *sql.DB
+}
+
+func NewVideoConverter(db *sql.DB) *VideoConverter {
+	return &VideoConverter{
+		db: db,
+	}
+}
+
+// {"video_id": 1, "path": "media/uploads/1"}
+type VideoTask struct {
+	VideoID int    `json:"video_id"`
+	Path    string `json:"path"`
+}
+
+func (vc *VideoConverter) Handle(msg []byte) {
+	var task VideoTask
+	err := json.Unmarshal(msg, &task)
+	if err != nil {
+		vc.logError(task, "failed to unmarshal task", err)
+		return
+	}
+
+	if IsProcessed(vc.db, task.VideoID) {
+		slog.Warn("Video already processed", slog.Int("video_id", task.VideoID))
+		return
+	}
+
+	err = vc.processVideo(&task)
+	if err != nil {
+		vc.logError(task, "failed to process video", err)
+		return
+	}
+
+	err = MarkProcessed(vc.db, task.VideoID)
+	if err != nil {
+		vc.logError(task, "failed to mark video as processed", err)
+		return
+	}
+	slog.Info("Video marked as processed", slog.Int("video_id", task.VideoID))
+}
+
+func (vc *VideoConverter) processVideo(task *VideoTask) error {
+	mergedFile := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+	err := vc.mergeChunks(task.Path, mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to merge chunks", err)
+		return fmt.Errorf("failed to merge chunks: %v", err)
+	}
+
+	err = os.MkdirAll(mpegDashPath, os.ModePerm)
+	if err != nil {
+		vc.logError(*task, "failed to create mpeg-dash directory", err)
+		return err
+	}
+
+	slog.Info("Converting video to mpeg-dash", slog.String("path", task.Path))
+	ffmpegCmd := exec.Command(
+		"ffmpeg", "-i", mergedFile,
+		"-f", "dash",
+		filepath.Join(mpegDashPath, "output.mpd"),
+	)
+
+	output, err := ffmpegCmd.CombinedOutput()
+	if err != nil {
+		vc.logError(*task, "failed to convert video to mpeg-dash, output: "+string(output), err)
+		return err
+	}
+	slog.Info("Video converted to mpeg-dash", slog.String("path", mpegDashPath))
+
+	slog.Info("Removing merged file", slog.String("path", mergedFile))
+	err = os.Remove(mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to remove merged file", err)
+		return err
+	}
+	return nil
+}
+
+func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
+	errorData := map[string]any{
+		"video_id": task.VideoID,
+		"error":    message,
+		"datails":  err.Error(),
+		"time":     time.Now(),
+	}
+	serializedError, _ := json.Marshal(errorData)
+	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
+
+	RegisterError(vc.db, errorData, err)
+}
+
+func (vc *VideoConverter) extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName)) // string
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func (vc *VideoConverter) mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+	if err != nil {
+		return fmt.Errorf("failed to find chunks: %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return vc.extractNumber(chunks[i]) < vc.extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("failed to open chunk file: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("failed to write chunk %s to merged file: %v", chunk, err)
+		}
+		input.Close()
+	}
+	return nil
+}
